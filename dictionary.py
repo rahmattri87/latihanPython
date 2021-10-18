de659188791b7441456bfbf32d2ec0fe66938ac7
Rahmat Tri Yunandar <rahmattri.yunandar@gmail.com>
2021-10-18 19:22:17 +0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -17,67 +17,67 @@ print(my_dict.get('address'))
 # print(my_dict['address'])
 
 
-# # ==== Changing and Adding Dictionary elements ====
+# ==== Changing and Adding Dictionary elements ====
 
-# # Changing and adding Dictionary Elements
-# my_dict = {'name': 'Jack', 'age': 26}
+# Changing and adding Dictionary Elements
+my_dict = {'name': 'Jack', 'age': 26}
 
-# # update value
-# my_dict['age'] = 27
+# update value
+my_dict['age'] = 27
 
-# #Output: {'age': 27, 'name': 'Jack'}
-# print(my_dict)
+#Output: {'age': 27, 'name': 'Jack'}
+print(my_dict)
 
-# # add item
-# my_dict['address'] = 'Downtown'
+# add item
+my_dict['address'] = 'Downtown'
 
-# # Output: {'address': 'Downtown', 'age': 27, 'name': 'Jack'}
-# print(my_dict)
+# Output: {'address': 'Downtown', 'age': 27, 'name': 'Jack'}
+print(my_dict)
 
 
-# # ==== Removing elements from Dictionary ====
+# ==== Removing elements from Dictionary ====
 
-# # Removing elements from a dictionary
+# Removing elements from a dictionary
 
-# # create a dictionary
-# squares = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+# create a dictionary
+squares = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
 
-# # remove a particular item, returns its value
-# # Output: 16
-# print(squares.pop(4))
+# remove a particular item, returns its value
+# Output: 16
+print(squares.pop(4))
 
-# # Output: {1: 1, 2: 4, 3: 9, 5: 25}
-# print(squares)
+# Output: {1: 1, 2: 4, 3: 9, 5: 25}
+print(squares)
 
-# # remove an arbitrary item, return (key,value)
-# # Output: (5, 25)
-# print(squares.popitem())
+# remove an arbitrary item, return (key,value)
+# Output: (5, 25)
+print(squares.popitem())
 
-# # Output: {1: 1, 2: 4, 3: 9}
-# print(squares)
+# Output: {1: 1, 2: 4, 3: 9}
+print(squares)
 
-# # remove all items
-# squares.clear()
+# remove all items
+squares.clear()
 
-# # Output: {}
-# print(squares)
+# Output: {}
+print(squares)
 
-# # delete the dictionary itself
-# del squares
+# delete the dictionary itself
+del squares
 
-# # Throws Error
-# print(squares)
+# Throws Error
+print(squares)
 
-# # ==== Python Dictionary Methods ====
+# ==== Python Dictionary Methods ====
 
-# # Dictionary Methods
-# marks = {}.fromkeys(['Math', 'English', 'Science'], 0)
+# Dictionary Methods
+marks = {}.fromkeys(['Math', 'English', 'Science'], 0)
 
-# # Output: {'English': 0, 'Math': 0, 'Science': 0}
-# print(marks)
+# Output: {'English': 0, 'Math': 0, 'Science': 0}
+print(marks)
 
-# for item in marks.items():
-#     print(item)
+for item in marks.items():
+    print(item)
 
-# # Output: ['English', 'Math', 'Science']
-# print(list(sorted(marks.keys())))
+# Output: ['English', 'Math', 'Science']
+print(list(sorted(marks.keys())))
